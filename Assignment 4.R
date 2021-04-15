b4fcdd8b4873af60fdcf534d0a58c545334be408
jtd224 <78614983+jtd224@users.noreply.github.com>
2021-04-14 21:27:42 -0400

    Add files via upload

--- Assignment 4.R
+++ Assignment 4.R
@@ -1,11 +1,24 @@
+library(tidyverse)
+
 survey <- read.csv("city-of-st-louis-survey.csv")
 decode <- read.csv("city-of-st-louis-decode.csv")
 
 survey <- select(survey,owner, Land, Area)
 
-survey$First <- substr(survey$owner,1,1)
-survey$First
+firstLetter <- substr(survey$owner,1,1)
+firstLetter
 
-survey_A <-survey[which(survey$First == "A")]
+survey_reduced<-survey[which(firstLetter=="A"),]
 
-YT_Sample_validated <- merge(out2, YT_Sample_validated, by.x="id", by.y="id")
+ownerIndex<-match(survey_reduced$Owner, decode$Original.Name)
+
+survey_reduced$Owner[!is.no(owneerIndex)]<-decode$Standardized.Names[ownerIndex[!is.na(ownerIndex)]]
+
+data1<-tapply(survey_reduced$Land.Area, INDEX = survey_reduced$Owner, FUN = sum)
+
+data1Sorted<-sort(data1, decreasing = TRUE)
+
+data1SortedDF<-data.frame(data1Sorted)
+
+head(data1SortedDF, n=10)
+
